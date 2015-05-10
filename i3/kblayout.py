59d68e625cf976006236ebab041edb33ad33e0e8
Squeaky <squeaky_pl@gmx.com>
2015-05-10 16:08:35 +0200

    kblayout, i3exit and initial config

---
+++ i3/kblayout.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import subprocess
+
+
+layouts = [('us', 'altgr-intl'), ('pl', None)]
+
+
+def get_current_layout():
+    split = subprocess.check_output(['setxkbmap', '-query']).split()
+    pairs = dict(zip((k[:-1] for k in split[::2]), split[1::2]))
+    return pairs['layout'], pairs.get('variant')
+
+
+def set_layout(layout, variant):
+    cmd = ['setxkbmap', '-layout', layout]
+    if variant:
+        cmd.extend(['-variant', variant])
+    subprocess.check_call(cmd)
+
+
+def toggle():
+    try:
+        next_layout = layouts[
+            (layouts.index(get_current_layout()) + 1) % len(layouts)]
+    except ValueError:
+        next_layout = layouts[0]
+
+    set_layout(*next_layout)
+
+    return next_layout
+
+
+if __name__ == '__main__':
+    print(toggle())
