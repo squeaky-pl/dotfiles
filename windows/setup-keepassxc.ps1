71dbee0b5447c3673782e60d4c5e2ef123ac9c0c
squeaky <showerproof86@gmail.com>
2023-10-21 18:09:51 +0200

    browser configuration for keepass

--- windows/setup-keepassxc.ps1
+++ windows/setup-keepassxc.ps1
@@ -1,9 +1,23 @@
 scoop bucket add extras
 scoop install extras/keepassxc
 
-Copy-Item $PSScriptRoot/KeePassXC/keepassxc.ini `
+# This seems to be the first folder where configuration is looked at, at least for scoop
+Copy-Item $PSScriptRoot\KeePassXC\keepassxc.ini `
     -Destination (scoop which keepassxc).Replace('~', $env:USERPROFILE).Replace('KeePassXC.exe', 'config\keepassxc.ini') `
     -Force
+
+# This is the standard folder, it has browser configuration
+Copy-Item $PSScriptRoot/KeePassXC `
+    -Destination $env:LOCALAPPDATA `
+    -Recurse `
+    -Force
+
+# The browser configuration files contain `~` which we need to replace with user profile
+$browser_configs = Get-ChildItem -Path $env:LOCALAPPDATA\KeePassXC -Filter "org.keepassxc.keepassxc_browser_*"
+foreach ($browser_config in $browser_configs)
+{
+    ((Get-Content -path $browser_config.FullName -Raw) -replace '~', $env:USERPROFILE.replace('\', '\\')) | Set-Content -Path $browser_config.FullName
+}
 
 Start-Process -NoNewWindow -FilePath (scoop which keepassxc).Replace('~', $env:USERPROFILE) | Out-Null
 
