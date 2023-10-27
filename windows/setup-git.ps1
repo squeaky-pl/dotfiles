0be6c098d4f5b8dc522a063509629b7977f83788
Squeaky <squeaky_pl@gmx.com>
2023-10-27 19:16:34 +0200

    Start ssh-agent for git

--- windows/setup-git.ps1
+++ windows/setup-git.ps1
@@ -1,2 +1,11 @@
 scoop bucket add main
 scoop install main/git
+scoop install sudo
+scoop install main/delta
+
+sudo powershell -Command "Get-Service -Name ssh-agent | Set-Service -StartupType Automatic; Start-Service ssh-agent"
+
+if(!(Test-Path $env:USERPROFILE\.gitconfig))
+{
+    cmd /c mklink $env:USERPROFILE\.gitconfig $PSScriptRoot\..\.gitconfig
+}
