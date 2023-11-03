b4b7b10e07c58add51333d19abb57a3f0976ed3c
Squeaky <squeaky_pl@gmx.com>
2023-11-03 14:09:07 +0100

    Install helix for git

--- windows/setup-git.ps1
+++ windows/setup-git.ps1
@@ -2,8 +2,9 @@ scoop bucket add main
 scoop install main/git
 scoop install sudo
 scoop install main/delta
+scoop install helix
 
-sudo powershell -Command "Get-Service -Name ssh-agent | Set-Service -StartupType Automatic; Start-Service ssh-agent"
+sudo powershell -NoProfile -Command "Get-Service -Name ssh-agent | Set-Service -StartupType Automatic; Start-Service ssh-agent"
 
 if(!(Test-Path $env:USERPROFILE\.gitconfig))
 {
