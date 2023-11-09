d017bdd795eef1b5d3ebdf291474566549b8fb6d
Squeaky <squeaky_pl@gmx.com>
2023-11-09 19:19:34 +0100

    fixes

--- windows/setup-glazewm.ps1
+++ windows/setup-glazewm.ps1
@@ -7,7 +7,7 @@ New-Item -ItemType Directory -Force -Path $env:USERPROFILE\.glaze-wm | Out-Null
 
 if(!(Test-Path $env:USERPROFILE\.glaze-wm\config.yaml))
 {
-    cmd /c mklink $env:USERPROFILE\.glaze-wm\config.yaml $PSScriptRoot\.glaze-wm\config.yaml
+    sudo cmd /c mklink $env:USERPROFILE\.glaze-wm\config.yaml $PSScriptRoot\.glaze-wm\config.yaml
 }
 
 New-Autostart-Item (Get-Scoop-App-Location glazewm)
