f4c21f6269f98d2bf07bc0daf4461de41de6afef
squeaky <showerproof86@gmail.com>
2023-11-09 11:47:19 +0100

    Update setup-self

--- windows/setup-self.ps1
+++ windows/setup-self.ps1
@@ -1,1 +1,9 @@
-git clone https://github.com/squeaky-pl/dotfiles.git $env:USERPROFILE/dotfiles
+if(!(Get-Command scoop -errorAction SilentlyContinue)) {
+    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+}
+scoop install git
+if(!(Test-Path $env:USERPROFILE/dotfiles)) {
+    git clone https://github.com/squeaky-pl/dotfiles.git $env:USERPROFILE/dotfiles
+}
+
+Write-Output "Go to $env:USERPROFILE\dotfiles\windows"
