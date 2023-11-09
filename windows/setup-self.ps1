f52ba1003a79b2eb212701a8732356895e04729a
Squeaky <squeaky_pl@gmx.com>
2023-11-09 18:08:18 +0100

    update self

--- windows/setup-self.ps1
+++ windows/setup-self.ps1
@@ -1,7 +1,13 @@
 if(!(Get-Command scoop -errorAction SilentlyContinue)) {
     Invoke-RestMethod get.scoop.sh | Invoke-Expression
 }
+
+if(!(Get-Command winget -errorAction SilentlyContinue)) {
+    scoop install winget
+}
+
 scoop install git
+
 if(!(Test-Path $env:USERPROFILE/dotfiles)) {
     git clone https://github.com/squeaky-pl/dotfiles.git $env:USERPROFILE/dotfiles
 }
