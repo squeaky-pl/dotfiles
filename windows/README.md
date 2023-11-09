c15af6c360fa5d906b8513829f04d86a46d3dca5
squeaky <showerproof86@gmail.com>
2023-11-09 13:48:56 +0100

    Improve setup-git

--- windows/README.md
+++ windows/README.md
@@ -5,3 +5,11 @@ Drop to Powershell and run:
 ```
 irm https://raw.githubusercontent.com/squeaky-pl/dotfiles/master/windows/setup-self.ps1 | iex
 ```
+
+Then most likely you want to
+
+```
+windows\setup-keepassxc.ps1
+windows\setup-box.ps1
+windows\setup-git.ps1
+```
--- windows/setup-git.ps1
+++ windows/setup-git.ps1
@@ -6,7 +6,20 @@ scoop install helix
 
 sudo powershell -NoProfile -Command "Get-Service -Name ssh-agent | Set-Service -StartupType Automatic; Start-Service ssh-agent"
 
-if(!(Test-Path $env:USERPROFILE\.gitconfig))
+git remote set-url origin git@github.com:squeaky-pl/dotfiles.git
+
+New-Item -ItemType Directory -Path $env:USERPROFILE/.ssh -Force
+
+if(!(Test-Path $env:USERPROFILE/.ssh/id_rsa) -and (Test-Path Y:\database.kdbx)) {
+    keepassxc-cli.exe attachment-export Y:\database.kdbx "SSH/public private key" id_rsa $env:USERPROFILE/.ssh/id_rsa
+}
+
+if(Test-Path $env:USERPROFILE/.ssh/id_rsa) {
+    ssh-add $env:USERPROFILE/.ssh/id_rsa
+    ssh -T git@github.com
+}
+
+if(!(Test-Path $env:USERPROFILE/.gitconfig))
 {
     cmd /c mklink $env:USERPROFILE\.gitconfig $PSScriptRoot\..\.gitconfig
 }
