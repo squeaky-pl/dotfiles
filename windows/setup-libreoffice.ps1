922b89deb8e70389e914340c3fa81a541516a5a6
Squeaky <squeaky_pl@gmx.com>
2023-11-11 18:23:00 +0100

    use winget to install libreoffice

--- windows/setup-libreoffice.ps1
+++ windows/setup-libreoffice.ps1
@@ -1,4 +1,5 @@
-scoop bucket add extras
-scoop install libreoffice
+. $PSScriptRoot\utils.ps1
+
+Install-Winget-App TheDocumentFoundation.LibreOffice
 
 Copy-Item $PSScriptRoot\LibreOffice -Destination $env:APPDATA -Recurse -Force
