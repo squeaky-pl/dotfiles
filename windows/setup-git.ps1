901993d3b0ea9fc2b5a97a7886455303fdfe71ed
Squeaky <squeaky_pl@gmx.com>
2023-12-12 09:43:32 +0100

    GitHub cli

--- windows/setup-git.ps1
+++ windows/setup-git.ps1
@@ -1,8 +1,12 @@
+. $PSScriptRoot\utils.ps1
+
 scoop bucket add main
 scoop install main/git
 scoop install sudo
 scoop install main/delta
 scoop install helix
+
+Install-Winget-App GitHub.cli
 
 sudo powershell -NoProfile -Command "Get-Service -Name ssh-agent | Set-Service -StartupType Automatic; Start-Service ssh-agent"
 
