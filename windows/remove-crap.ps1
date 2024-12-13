79d1ce37755599e55f7868de740f052f6bc9fe2e
Squeaky <squeaky_pl@gmx.com>
2024-12-13 19:36:58 +0100

    More dev, bcul

--- windows/remove-crap.ps1
+++ windows/remove-crap.ps1
@@ -1,4 +1,5 @@
-scoop bucket add extras
-scoop install extras/bulk-crap-uninstaller
+. $PSScriptRoot\utils.ps1
 
-bcu-console uninstall $PSScriptRoot\crap.bcul /Q /U /V
+Install-Winget-App Klocman.BulkCrapUninstaller
+
+& 'C:\Program Files\BCUninstaller\win-x64\BCU-console.exe' uninstall $PSScriptRoot\crap.bcul /Q /U /V
